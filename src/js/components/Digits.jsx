7a1b8b5916707fc0057823f761bbc36c01a0d2f4
Oliver Simkovsky <53585119+fluffbutt1@users.noreply.github.com>
2026-02-10 01:06:44 +0000

    changes to digits component. rendered first two columns for counter

---
+++ src/js/components/Digits.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Digits = ({value}) => {
+
+    return (
+        <h1>{value}</h1>
+    );
+};
+
+export default Digits;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import Digits from "./Digits";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="d-flex flex-column align-items-center">
-            <div className="bg-primary w-75 text-center">
-				<div className="row border">
-					<div className="col-3 bg-danger border rounded">hi</div>
-					<div className="col-3 bg-danger border rounded">hi</div>
-					<div className="col-3 bg-danger border rounded">hi</div>
-					<div className="col-3 bg-danger border rounded">hi</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+    console.log(counter);
+    clearTimeout();
+  }, 1000);
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <div className="bg-primary w-75 text-center">
+        <div className="row border">
+          <div className="col-3 bg-danger border rounded">
+            hi
+          </div>
+          <div className="col-3 bg-danger border rounded">hi</div>
+          <div className="col-3 bg-danger border rounded">
+			<Digits value={Math.floor(counter % 100 / 10)} />
+		  </div>
+          <div className="col-3 bg-danger border rounded">
+			<Digits value={counter % 10} />
+		  </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
